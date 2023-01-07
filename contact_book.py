831244f66f5256d4ca791774eb8a953d28f6af79
JuppLCD <lorenzocanovas207@gmail.com>
2023-01-07 19:01:11 -0300

    Create class ContactBook and DBFile

---
+++ contact_book.py
@@ -0,0 +1,155 @@
+import os
+from file_db import DBFile
+
+
+class ContactBook:
+
+    OPTIONS = {
+        1: 'Add contact',
+        2: 'Show all contacts',
+        3: 'Edit contact',
+        4: 'Delete contact',
+        5: 'Find contact',
+        0: 'Close'
+    }
+    contact_book = {}
+
+    def __init__(self, db_file_name: str) -> None:
+        self.db_file = DBFile(db_file_name, self.contact_book)
+
+        self.method_to_execute = {
+            1: self.add_contact,
+            2: self.show_all_contacts,
+            3: self.edit_contact,
+            4: self.delete_contact,
+            5: self.find_contact
+        }
+
+    def start(self):
+        loop = True
+
+        while loop:
+            self.show_menu()
+            opt = self.select_option()
+
+            if (opt in self.OPTIONS):
+                if (opt == 0):
+                    loop = False
+                    print('Bye, have a nice day')
+                # elif (opt == 1):
+                #     self.add_contact()
+                # elif (opt == 2):
+                #     self.show_all_contacts()
+                # elif (opt == 3):
+                #     self.edit_contact()
+                # elif (opt == 4):
+                #     self.delete_contact()
+                # elif (opt == 5):
+                #     self.find_contact()
+                else:
+                    self.method_to_execute[opt]()
+            else:
+                print('Invalid option')
+
+            if (loop):
+                input('Press to continue...')
+
+    def create_section_title(self, title: str) -> None:
+        os.system('clear')
+        print(f'							{title}')
+
+    def show_menu(self) -> None:
+        self.create_section_title('MENU')
+        for key, value in self.OPTIONS.items():
+            print(f'{key} ) {value}')
+
+    def select_option(self) -> int:
+        opt = input('Select an option: ')
+        try:
+            opt = int(opt)
+        except ValueError:
+            opt = -1
+        return opt
+
+    def add_contact(self) -> None:
+        self.create_section_title(self.OPTIONS[1].upper())
+
+        name = input('Name: ')
+
+        if self.contact_book.get(name):
+            print('There is already a contact with that name')
+        else:
+            email = input('Email: ')
+            phone = input('Phone: ')
+
+            self.contact_book.setdefault(name, (email, phone))
+            self.db_file.add_contact((name, email, phone))
+
+            print('The contact was created successfully')
+
+    def show_all_contacts(self) -> None:
+        self.create_section_title(self.OPTIONS[2].upper())
+
+        if (len(self.contact_book) == 0):
+            print('There are no contacts.')
+        else:
+            print(f'There are {len(self.contact_book)} scheduled contacts \n')
+            for name, contact in self.contact_book.items():
+                self.show_contact(name, contact[0], contact[1])
+
+    def edit_contact(self) -> None:
+        self.create_section_title(self.OPTIONS[3].upper())
+
+        if (len(self.contact_book) == 0):
+            print('There are no contacts.')
+        else:
+            contact_to_edit = input('Name: ')
+            if (self.contact_book.get(contact_to_edit)):
+                email = input('Email: ')
+                phone = input('Phone: ')
+
+                self.contact_book[contact_to_edit] = (email, phone)
+                self.db_file.refreshing_contacts(self.contact_book)
+                print('Contact successfully edited')
+            else:
+                print(f'No contacts found with the name: {contact_to_edit}.')
+
+    def delete_contact(self) -> None:
+        self.create_section_title(self.OPTIONS[4].upper())
+
+        if (len(self.contact_book) == 0):
+            print('There are no contacts.')
+        else:
+            contact_to_delete = input('Name: ')
+            if (self.contact_book.get(contact_to_delete)):
+                self.contact_book.pop(contact_to_delete)
+                self.db_file.refreshing_contacts(self.contact_book)
+                print('Contact deleted successfully')
+            else:
+                print(f'No contacts found with the name: {contact_to_delete}.')
+
+    def find_contact(self) -> None:
+        self.create_section_title(self.OPTIONS[5].upper())
+
+        if (len(self.contact_book) == 0):
+            print('There are no contacts.')
+        else:
+            find_contact = input('Name: ')
+            contacts_match = []
+            for name, contact in self.contact_book.items():
+                if (find_contact.lower() in name.lower()):
+                    contacts_match.append((name, contact))
+
+            if (len(contacts_match) == 0):
+                print(f'No contacts found with the name: {find_contact}.')
+            else:
+                print(f'There are {len(contacts_match)} scheduled contacts \n')
+                for name, contact in contacts_match:
+                    self.show_contact(name, contact[0], contact[1])
+
+    def show_contact(self, name, email: str, phone: str) -> None:
+        print('*'*10)
+        print(f'Name: {name}')
+        print(f'Email: {email}')
+        print(f'Phone: {phone}')
+        print('*'*10)
--- file_db.py
+++ file_db.py
@@ -1,20 +1,26 @@
-import pathlib
+class DBFile:
+    def __init__(self, file_name: str, contact_book: dir):
+        self.file_name = file_name
 
+        self.load_contacts(contact_book)
 
-def load_contacts_in_db(contact_book: dict, file_name: str) -> None:
-    if pathlib.Path(file_name).exists():
-        with open(file_name, 'r') as db_file:
-            for line in db_file:
-                name, email, phone = line.strip().split(',')
-                contact_book.setdefault(name, (email, phone))
-    else:
-        with open(file_name, 'w') as db_file:
+    def load_contacts(self, contact_book: dir) -> None:
+        try:
+            with open(self.file_name, 'r') as db_file:
+                for line in db_file:
+                    name, email, phone = line.strip().split(',')
+                    contact_book.setdefault(name, (email, phone))
+        except FileNotFoundError:
+            with open(self.file_name, 'w') as db_file:
+                pass
+
+    def refreshing_contacts(self, contact_book: dict) -> None:
+        with open(self.file_name, 'w') as db_file:
             pass
+        with open(self.file_name, 'a') as db_file:
+            for name, data in contact_book.items():
+                db_file.write(f'{name},{data[0]},{data[1]}\n')
 
-
-def refreshing_contacts_in_db(contact_book: dict, file_name: str) -> None:
-    with open(file_name, 'w') as db_file:
-        pass
-    with open(file_name, 'a') as db_file:
-        for name, data in contact_book.items():
-            db_file.write(f'{name},{data[0]},{data[1]}\n')
+    def add_contact(self, contact: tuple):
+        with open(self.file_name, 'a') as db_file:
+            db_file.write(f'{contact[0]},{contact[1]},{contact[2]}\n')
